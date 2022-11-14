ac2ad967097504cf4ec941b4da2250bc00bd6193
Andriy <you@example.com>
2022-11-14 12:11:51 +0200

    Verion 1.0.0

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import Container from "@mui/material/Container";
+import { Routes, Route } from "react-router-dom";
+import { useDispatch } from "react-redux";
+
+import { Header } from "./components";
+import { Home, FullPost, Registration, AddPost, Login, SimilarTags } from "./pages";
+import { fetchAuthMe } from "./redux/slices/auth";
+import React from "react";
+
+function App() {
+  const dispatch = useDispatch();
+
+  React.useEffect(() => {
+    dispatch(fetchAuthMe());
+  }, []);
+
+  return (
+    <>
+      <Header />
+      <Container maxWidth="lg">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/posts/:id" element={<FullPost />} />
+          <Route path="/tags/:name" element={<SimilarTags />} />
+          <Route path="/posts/:id/edit" element={<AddPost />} />
+          <Route path="/add-post" element={<AddPost />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Registration />} />
+        </Routes>
+      </Container>
+    </>
+  );
+}
+
+export default App;

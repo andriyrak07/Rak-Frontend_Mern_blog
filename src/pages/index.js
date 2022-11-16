67f854ed7898eacf3c911d87ccebc6a6620c2052
Andriy <you@example.com>
2022-11-16 19:10:16 +0200

    Clear file

--- src/pages/index.js
+++
@@ -1,6 +0,0 @@
-export { Home } from "./Home";
-export { SimilarTags } from "./SimilarTags";
-export { FullPost } from "./FullPost";
-export { AddPost } from "./AddPost";
-export { Registration } from "./Registration";
-export { Login } from "./Login";
